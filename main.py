ce83a59c779b894666753ccadffd0603fe5343dc
Alex Lytvynenko <oleksandr.lytvynenko@codeit.pro>
2023-04-14 13:06:18 +0300

    ~ port

--- main.py
+++ main.py
@@ -102,5 +102,5 @@ def handler(exception):
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 # [END gae_flex_quickstart]
